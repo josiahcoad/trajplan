5d01fb9f58cc350d8bad93e399e9094238f467db
Josiah Coad <josiah@coad.net>
2020-10-22 01:32:38 -0500

    added pretraining

--- pretraining.py
+++ pretraining.py
@@ -1,42 +1,44 @@
-from stable_baselines import PPO2
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+from stable_baselines import SAC
 from stable_baselines.gail import generate_expert_traj, ExpertDataset
-from behavioral import get_behav
+from behavioral import get_behav, NoPathError
 from main import Env
+import numpy as np
+from state import State
 
-def generate_expert():
-    env = Env()
+# generate expert trajectory
+def expert(obs):
+    try:
+        state = State(Env.depth, Env.width).load_obs(obs)
+        return get_behav(state, weights={'fr': 0.3})
+    except NoPathError:
+        return np.zeros(Env.depth*2)
 
-    def expert(obs):
-        return get_behav(obs, weights=[.1,1,1,1,1,10,1])
-
-    generate_expert_traj(expert, 'expert', env, n_episodes=10)
+# generate_expert_traj(expert, 'expert', Env(), n_episodes=100)
 
 
-# Using only one expert trajectory
-# you can specify `traj_limitation=-1` for using the whole dataset
-dataset = ExpertDataset(expert_path='expert_cartpole.npz',
-                        traj_limitation=1, batch_size=128)
-
-model = PPO2('MlpPolicy', 'CartPole-v1', verbose=1)
-# Pretrain the PPO2 model
-model.pretrain(dataset, n_epochs=1000)
-
-# As an option, you can train the RL agent
-# model.learn(int(1e5))
+# pretrain model
+dataset = ExpertDataset(expert_path='expert.npz')
+model = SAC('MlpPolicy', Env(), verbose=1)
+model.pretrain(dataset, n_epochs=5000)
+model.save('pretrained_sac')
 
 # Test the pre-trained model
 env = model.get_env()
 obs = env.reset()
 
-reward_sum = 0.0
-for _ in range(1000):
-        action, _ = model.predict(obs)
-        obs, reward, done, _ = env.step(action)
-        reward_sum += reward
-        env.render()
-        if done:
-                print(reward_sum)
-                reward_sum = 0.0
-                obs = env.reset()
+reward_sum = 0
+i = 0
+for j in range(1000):
+    action, _ = model.predict(obs)
+    obs, reward, done, _ = env.step(action)
+    reward_sum += reward
+    i += 1
+    if done:
+        print(reward_sum, i, reward_sum / i)
+        reward_sum = 0
+        i = 0
+        obs = env.reset()
 
 env.close()
