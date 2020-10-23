716b5ae93aac25f24c826f06e03dff00b71a8968
Josiah Coad <josiah@coad.net>
2020-10-23 01:29:04 -0500

    better results plotting

--- plot_train.py
+++ plot_train.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import numpy as np
-
-df = pd.read_csv('logs/training/monitor.csv')
-
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from stable_baselines.bench.monitor import load_results
 
-s = (df.r / df.l)
-s = s[s != 0].rolling(1000).mean()
-plt.plot(np.cumsum(df.l)[-len(s):], s)
-plt.show()
 
-# results_plotter.plot_results([log_dir], time_steps, results_plotter.X_TIMESTEPS, "DDPG LunarLander")
-# plt.show()
+
+def plot_train(folder):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+    def animate():
+        df = load_results(folder)
+        df = df.iloc[100:]
+        df = df[df.r != 0]
+        s = (df.r / df.l)
+        s = s.rolling(100).mean()
+        ax1.clear()
+        ax1.scatter(np.cumsum(df.l), df.r / df.l, s=.1)
+        ax1.plot(np.cumsum(df.l)[-len(s):], s)
+        ax1.set_ylim(-1, 2)
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    plt.show()
+
+plot_train(folder='logs/training')
